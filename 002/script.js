d11b03df452847866ad3c42c87369ff1626d9868
Kyle Kuo <kyle.tt.kuo@gmail.com>
2023-06-23 15:37:44 -0300

    feat: :art: code from 002 class

--- 002/script.js
+++ 002/script.js
@@ -1,54 +1,48 @@
 import { loaded } from '../@dependencies/js/LoadEvents.js';
-// import { createReadableStream } from './Stream.js';
-
 await loaded;
 
 // GRAPH
 
-const graph = document.querySelector('#graph'),
-			svg = d3.select(graph).append('svg');
+const svg = d3.select('#graph').append('svg');
 
-// let data = [ 1, 2, 3, 4, 5 ],
-// 		circle = svg.selectAll('circle');
+let data = [ 1, 2, 3, 4, 5 ],
+		circle = svg.selectAll('circle');
 
-// circle = circle
-// 	.data(data, d => d)
-// 	.join(
-// 		enter => enter
-// 			.append('circle')
-// 			.attr('r', d => d * 10)
-// 			.attr('cx', d => (d * 5) ** 2)
-// 			.attr('cy', d => (d * 5) ** 2)
-// 			.attr('fill', 'green')
-// 	);
+const updateCircles = (data) => {
 
-// setTimeout(() => {
+	circle = circle
+		.data(data, d => d)
+		.join(
+	
+			enter => enter
+				.append('circle')
+				.attr('r', d => d * 10)
+				.attr('cx', d => (d * 5) ** 2)
+				.attr('cy', d => (d * 5) ** 2)
+				.attr('fill', 'green'),
+	
+			update => update
+				.attr('fill', 'pink'),
+	
+			exit => exit
+				.attr('fill', 'red')
+				.transition(500)
+				.style('opacity', 0)
+				.transition(10)
+				.remove()
+	
+		);
 
-// 	circle = circle
-// 		.data(data, d => d)
-// 		.join(
-// 			enter => enter,
-// 			update => update
-// 				.attr('fill', 'blue')
-// 		);
+}
 
-// }, 1000);
+updateCircles(data);
 
-// setTimeout(() => {
+setTimeout(() => {
+	data = [ 1, 2, 3, 4, 5, 6 ];
+	updateCircles(data);
+}, 1000);
 
-// 	data = [ 1, 2, 3 ];
-
-// 	circle
-// 		.data(data, d => d)
-// 		.join(
-// 			enter => enter,
-// 			update => update,
-// 			exit => exit
-// 				.attr('fill', 'red')
-// 				.transition(500)
-// 				.style('opacity', 0)
-// 				.transition(10)
-// 				.remove()
-// 		);
-
-// }, 2000);
+setTimeout(() => {
+	data = [ 1, 2, 3, 6 ];
+	updateCircles(data);
+}, 2000);
