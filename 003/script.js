a18b56da77b48c31b179d4df30955b999076d311
Kyle Kuo <kyle.tt.kuo@gmail.com>
2023-06-30 16:16:21 -0300

    feat: :sparkles: adds code for class 003

---
+++ 003/script.js
@@ -0,0 +1,71 @@
+import { loaded } from '../@dependencies/js/LoadEvents.js';
+await loaded;
+
+// GRAPH
+
+const graph = document.querySelector('#graph'),
+			yearlyData = {};
+
+fetch('./data-003.json')
+	.then( function ( resp ) { 
+		return resp.json() 
+	})
+	.then( function ( data ) {
+
+		const scale = d3.scaleLinear()
+			.domain([ 0, 1371220000 ])
+			.range([ 0, 500 ]);
+
+		for (const country of data) {
+			for (const yearData of country.population) {
+				if ( !yearlyData[ yearData['year'] ] ) yearlyData[ yearData['year'] ] = {}
+				yearlyData[ yearData['year'] ][ country['name'] ] = yearData['count'];
+			}
+		}
+
+		for (const [year, data] of Object.entries(yearlyData)) {
+
+			const yearElement = d3.select(graph)
+				.append('div')
+				.classed('year', true);
+
+			const yearLabel = yearElement
+				.append('div')
+				.classed('year-label', true)
+				.text(year);
+
+			const yearDataWrap = yearElement
+				.append('div')
+				.classed('year-data', true);
+
+			console.log(data);
+
+			for (const [country, count] of Object.entries(data)) {
+
+				const countryData = yearDataWrap
+					.append('div')
+					.classed('country-data', true)
+					.style('--count', count)
+					.style('--scaled-count', scale( count ));
+
+				const countryLabel = countryData
+					.append('div')
+					.classed('country-label', true)
+					.text(country);
+
+				const countryCount = countryData
+					.append('div')
+					.classed('country-count', true)
+					.style('background', () => {
+						switch (country) {
+							case 'Brasil': return 'green';
+							case 'Estados Unidos': return 'blue';
+							case 'China': return 'red';
+						}
+					})
+					.text(count);
+			}
+
+		}
+
+	});
